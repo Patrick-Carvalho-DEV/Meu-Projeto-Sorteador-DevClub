4281986ca97f1aab5b68ee499f69e6122f220bba
Patrick-Carvalho-DEV <patrickcarvalhobh@gmail.com>
2024-08-06 20:07:37 -0300

    Meu primeiro commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function generateNumber() {
+
+    const min = Math.ceil(document.querySelector(".entre").value)
+    const max = Math.floor(document.querySelector(".e").value)
+
+    if (min >= max) {
+        alert('O valor minímo tem que ser MENOR que o valor máximo')
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert(result)
+    }
+
+
+
+}
